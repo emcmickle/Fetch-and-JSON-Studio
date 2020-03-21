b2771deb89175da99983c92d33218cbb41b7469f
Elizabeth McMickle <emartchink@yahoo.com>
2020-03-21 18:23:04 -0500

    studio 16 complete

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 // TODO: add code here
+
+window.addEventListener("load", function() {
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
+        response.json().then( function(json) {
+            const div = document.getElementById("container");
+            div.innerHTML+=`Number of astronauts: ${json.length}`;
+            for(let i = 0; i < json.length; i++) {
+                let astronaut = json[i];
+                div.innerHTML+=`<div class="astronaut">
+                    <div class="bio">
+                        <h3>${astronaut.firstName} ${astronaut.lastName}</h3>
+                        <ul>
+                            <li>Hours in Space: ${astronaut.hoursInSpace}</li>
+                            <li>Active: ${astronaut.active}</li>
+                            <li>Skills: ${astronaut.skills}</li>
+                        </ul>
+                    </div>
+                    <img class="avatar" src="${astronaut.picture}">
+                </div>`
+            }
+        });
+    });
+});
